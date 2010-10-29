ec21d349b605d70d5517580c9eb5ee105654ced4
jasonku <jasontku@gmail.com>
2010-10-28 19:10:00 -0700

    adding directives to person.rb model

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,8 +1,21 @@
 class Person < ActiveRecord::Base
+
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :lockable, :timeoutable, :confirmable and :activatable
+  devise :database_authenticatable, :registerable,
+    :recoverable, :rememberable, :trackable, :validatable
+
+
+
   has_and_belongs_to_many :arrangements, :join_table => "arrangements_passengers",
     :foreign_key => "passenger_id"
   has_many :trips, :foreign_key => "organizer_id"
   has_and_belongs_to_many :trips, :join_table => "participants_trips",
     :foreign_key => "participant_id"
   has_many :vehicles, :foreign_key => "owner_id"
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation
+
+
 end
