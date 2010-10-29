e20077a39fe100bea6b12de88ee7b22bff045400
jasonku <jasontku@gmail.com>
2010-10-28 19:30:49 -0700

    rake db:migrate runs without errors with devise integration migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101026043531) do
+ActiveRecord::Schema.define(:version => 20101029021446) do
 
   create_table "arrangements", :force => true do |t|
     t.datetime "created_at"
@@ -39,12 +39,25 @@ ActiveRecord::Schema.define(:version => 20101026043531) do
 
   create_table "people", :force => true do |t|
     t.string   "name"
-    t.string   "email"
     t.string   "phone"
     t.string   "address"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email",                               :default => "", :null => false
+    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
+    t.string   "password_salt",                       :default => "", :null => false
+    t.string   "reset_password_token"
+    t.string   "remember_token"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                       :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
+
+  add_index "people", ["email"], :name => "index_people_on_email", :unique => true
+  add_index "people", ["reset_password_token"], :name => "index_people_on_reset_password_token", :unique => true
 
   create_table "trips", :force => true do |t|
     t.time     "time"
