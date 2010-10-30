71a1b0e3d6c38a232f831889aa159664e9893354
jasonku <jasontku@gmail.com>
2010-10-30 14:49:19 -0700

    new devise routes with paths

--- config/routes.rb
+++ config/routes.rb
@@ -19,34 +19,36 @@ Rides::Application.routes.draw do
                 + Invite Participant (not yet)
 =end
 
-  devise_for :people, :skip => :sessions do
+  devise_for :people, :skip => [:sessions, :registration] do
+    # Session routes for Authenticatable (default)
+    #      new_person_session GET  /people/sign_in                    {:controller=>"devise/sessions", :action=>"new"}
+    #          person_session POST /people/sign_in                    {:controller=>"devise/sessions", :action=>"create"}
+    #  destroy_person_session GET  /people/sign_out                   {:controller=>"devise/sessions", :action=>"destroy"}
+
     get "/signin" => "devise/sessions#new", :as => "new_person_session"
     post "/signin" => "devise/sessions#create", :as => "person_session"
     get "/signout" => "devise/sessions#destroy", :as => "destroy_person_session"
-    get "/signup" => "devise/registrations#new"
+
+
+    #     person_registration POST   /people(.:format)               {:action=>"create", :controller=>"devise/registrations"}
+    # new_person_registration GET    /people/sign_up(.:format)       {:action=>"new", :controller=>"devise/registrations"}
+    #edit_person_registration GET    /people/edit(.:format)          {:action=>"edit", :controller=>"devise/registrations"}
+    #     person_registration PUT    /people(.:format)               {:action=>"update", :controller=>"devise/registrations"}
+    #     person_registration DELETE /people(.:format)               {:action=>"destroy", :controller=>"devise/registrations"
+    post "/people" => "devise/registrations#create", :as => "person_registration"
+    get "/signup" => "devise/registrations#new", :as => "new_person_registration"
+    get "/editprofile" => "devise/registrations#edit", :as => "edit_person_registration"
+    put "/people" => "devise/registrations#put", :as => "person_registration"
+    delete "/people" => "devise/registrations#destroy", :as => "person_registration"
+
+
+    #         person_password POST   /people/password(.:format)      {:action=>"create", :controller=>"devise/passwords"}
+    #     new_person_password GET    /people/password/new(.:format)  {:action=>"new", :controller=>"devise/passwords"}
+    #    edit_person_password GET    /people/password/edit(.:format) {:action=>"edit", :controller=>"devise/passwords"}
+    #         person_password PUT    /people/password(.:format)      {:action=>"update", :controller=>"devise/passwords"}
+
   end
 
-=begin
-  # Session routes for Authenticatable (default)
-  #      new_person_session GET  /people/sign_in                    {:controller=>"devise/sessions", :action=>"new"}
-  #          person_session POST /people/sign_in                    {:controller=>"devise/sessions", :action=>"create"}
-  #  destroy_person_session GET  /people/sign_out                   {:controller=>"devise/sessions", :action=>"destroy"}
-
-  # # Password routes for Recoverable, if person model has :recoverable configured
-  #     new_person_password GET  /people/password/new(.:format)     {:controller=>"devise/passwords", :action=>"new"}
-  #    edit_person_password GET  /people/password/edit(.:format)    {:controller=>"devise/passwords", :action=>"edit"}
-  #         person_password PUT  /people/password(.:format)         {:controller=>"devise/passwords", :action=>"update"}
-  #                       POST /people/password(.:format)         {:controller=>"devise/passwords", :action=>"create"}
-=end
-  # # Confirmation routes for Confirmable, if person model has :confirmable configured
-  # JKU: I don't think we need Confirmable.
-  # new_person_confirmation GET  /people/confirmation/new(.:format) {:controller=>"devise/confirmations", :action=>"new"}
-  #     person_confirmation GET  /people/confirmation(.:format)     {:controller=>"devise/confirmations", :action=>"show"}
-  #                       POST /people/confirmation(.:format)     {:controller=>"devise/confirmations", :action=>"create"}
-
-
-
-  #  devise_for :people, :path_names => { :sign_up => "new"}
 
   root :to => 'people#index'
 
