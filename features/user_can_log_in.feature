6cad3dc70ed75051fc43b73527c00fd35115b027
jasonku <jasontku@gmail.com>
2010-10-31 16:31:36 -0700

    web step definitions for testing log in with cucumber

--- features/user_can_log_in.feature
+++ features/user_can_log_in.feature
@@ -5,16 +5,14 @@ Feature: User can log in
   So that I use the service as myself
 
   Scenario: User logs in with valid credentials
-    Given I have an account with email "test@test.com" and password "testpassword"
-    And I am on the sign in page
+    Given I am on the sign in page
     When I fill in "person[email]" with "test@test.com"
     And I fill in "person[password]" with "testpassword"
     And I press "Sign in"
     Then I should be on the trips dashboard
 
   Scenario: User logs in with invalid credentials
-    Given I have an account with email "test@test.com" and password "testpassword"
-    And I am on the sign in page
+    Given I am on the sign in page
     And I fill in "person[email]" with "test@test.com"
     And I fill in "person[password]" with "wrongpassword"
     When I press "Sign in"
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -4,9 +4,9 @@ describe Trip do
   describe "when validating a trip" do
     it "should not allow a trip with no name" do
       @no_name_attributes = {
-        :time => Time.parse("Sun Oct 31 13:13 GMT 2010")
+        :time => Time.parse("Sun Oct 31 13:13 GMT 2010"),
         :address => "2650 Haste Street",
-        :city => "Berkeley"
+        :city => "Berkeley",
         :state => "CA"
       }
       
@@ -29,7 +29,7 @@ describe Trip do
     it "should not allow a trip with invalid time" do
       @invalid_time_attributes = {
         :name => "Epic Trip of Epicness",
-        :time => "This is not a time field sadly"
+        :time => "This is not a time field sadly",
         :address => "2650 Haste Street",
         :city => "Berkeley",
         :state => "CA"
@@ -42,7 +42,7 @@ describe Trip do
     it "should not allow a trip with no address" do
       @no_address_attributes = {
         :name => "Epic Trip of Epicness",
-        :time => Time.parse("Sun Oct 31 13:13 GMT 2010")
+        :time => Time.parse("Sun Oct 31 13:13 GMT 2010"),
         :city => "Berkeley",
         :state => "CA"
       }
@@ -78,7 +78,7 @@ describe Trip do
   it "should create a new instance given valid attributes" do
     @valid_attributes = {
       :name => "Epic Trip of Epicness",
-      :time => Time.parse("Sun Oct 31 13:13 GMT 2010")
+      :time => Time.parse("Sun Oct 31 13:13 GMT 2010"),
       :address => "2650 Haste Street",
       :city => "Berkeley",
       :state => "CA"
