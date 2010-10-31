173990bba0daee127e83a806ea0955b78767786b
Young Kim <shadowing71@gmail.com>
2010-10-31 16:39:08 -0700

    Updated trip spec again

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -4,9 +4,9 @@ describe Trip do
   describe "when validating a trip" do
     it "should not allow a trip with no name" do
       @no_name_attributes = {
-        :time => Time.parse("Sun Oct 31 13:13 GMT 2010")
+        :time => Time.parse("Sun Oct 31 13:13 GMT 2010"),
         :address => "2650 Haste Street",
-        :city => "Berkeley"
+        :city => "Berkeley",
         :state => "CA"
       }
       
@@ -29,7 +29,7 @@ describe Trip do
     it "should not allow a trip with invalid time" do
       @invalid_time_attributes = {
         :name => "Epic Trip of Epicness",
-        :time => "This is not a time field sadly"
+        :time => "Invalid time!",
         :address => "2650 Haste Street",
         :city => "Berkeley",
         :state => "CA"
@@ -42,7 +42,7 @@ describe Trip do
     it "should not allow a trip with no address" do
       @no_address_attributes = {
         :name => "Epic Trip of Epicness",
-        :time => Time.parse("Sun Oct 31 13:13 GMT 2010")
+        :time => Time.parse("Sun Oct 31 13:13 GMT 2010"),
         :city => "Berkeley",
         :state => "CA"
       }
@@ -57,7 +57,15 @@ describe Trip do
 #    end
     
     it "should not allow a trip with no city" do
-      pending
+      @no_address_attributes = {
+        :name => "Epic Trip of Epicness",
+        :time => Time.parse("Sun Oct 31 13:13 GMT 2010"),
+        :address => "2650 Haste Street",
+        :state => "CA"
+      }
+      
+      @trip = Trip.new(@no_address_attributes)
+      @trip.should_not be_valid
     end
     
 #### Implement this test for later milestone    
@@ -66,19 +74,28 @@ describe Trip do
 #    end
     
     it "should not allow a trip with no state" do
-      pending
+      @no_state_attributes = {
+        :name => "Epic Trip of Epicness",
+        :time => Time.parse("Sun Oct 31 13:13 GMT 2010"),
+        :address => "2650 Haste Street",
+        :city => "Berkeley"
+      }
+      
+      @trip = Trip.new(@no_state_attributes)
+      @trip.should_not be_valid
     end
 
 #### Implement this test for later milestone    
 #    it "should not allow a trip with invalid state" do
 #      pending
-#    end    
+#    end
+    
   end
   
   it "should create a new instance given valid attributes" do
     @valid_attributes = {
       :name => "Epic Trip of Epicness",
-      :time => Time.parse("Sun Oct 31 13:13 GMT 2010")
+      :time => Time.parse("Sun Oct 31 13:13 GMT 2010"),
       :address => "2650 Haste Street",
       :city => "Berkeley",
       :state => "CA"
@@ -88,6 +105,14 @@ describe Trip do
   end
   
   it "should not create a new instance given invalid attributes" do
-    pending
+    @valid_attributes = {
+      :name => "Rage!@!@%^%$^%$&%$&",
+      :time => "Invalid Time",
+      :address => "2650 Haste Street",
+      :city => "Berkeley",
+      :state => "CA"
+    }
+    
+    Trip.create(@invalid_attributes).should be_false
   end
 end
