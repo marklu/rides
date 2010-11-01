3bc91700f02dae0c8bca7ab65b92be85e3ff4f5d
Mark Lu <marklu@marklu.net>
2010-10-31 17:04:23 -0700

    Removed bad step definition from global.rb

--- features/step_definitions/global.rb
+++ features/step_definitions/global.rb
@@ -9,20 +9,6 @@ Given /^I am logged in as a user$/ do
   click_button("Sign in")
 end
 
-Given /^(?:|I )have an account with email "([^"]*)" and password "([^"]*)"$/ do |email, password|
-  visit "/signout"
-  visit "/signup"
-  fill_in("Email", :with => "test@test.com")
-  fill_in("Password", :with => "testpassword")
-  fill_in("Password confirmation", :with => "testpassword")
-  fill_in("Name", :with => "Test Name")
-  fill_in("Phone", :with => "123-Test-Phone")
-  fill_in("Address", :with => "123 Test Street")
-  fill_in("City", :with => "Testville")
-  fill_in("State", :with => "CA")
-  click_button("Sign up")
-  visit "/signout"
-
 Given /^I am not authenticated$/ do
   visit('/signout') # ensure that at least
 end
