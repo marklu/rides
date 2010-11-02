69f4b54d829781283e9f0b6b2560ba9409d5f40b
Vinson Chuong <Vinson.Chuong@gmail.com>
2010-11-01 18:30:48 -0700

    "User can manage vehicles" complete.

--- features/step_definitions/user_can_manage_vehicles.rb
+++ features/step_definitions/user_can_manage_vehicles.rb
@@ -1,3 +1,7 @@
-Given /^I have added my "([^"]*)" passenger "([^"]*)" "([^"]*)"$/ do |arg1, arg2, arg3|
-  pending
+Given /^I have added my "([^"]*)" passenger "([^"]*)" "([^"]*)"$/ do |capacity, make, model|
+  visit path_to("the add vehicle page")
+  fill_in('Make', :with => make)
+  fill_in('Model', :with => model)
+  fill_in('Capacity', :with => capacity)
+  click_button('Create Vehicle')
 end
