fdbee77915d516fd0ee66c9752c893255a0351f9
Vinson Chuong <Vinson.Chuong@gmail.com>
2010-11-01 22:36:08 -0700

    Cleaned up spec tests. Added rcov

--- spec/requests/trips_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe "Trips" do
-  describe "GET /trips" do
-    it "works! (now write some real specs)" do
-      get trips_path
-    end
-  end
-end
--- spec/requests/vehicles_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe "Vehicles" do
-  describe "GET /vehicles" do
-    it "works! (now write some real specs)" do
-      get vehicles_path
-    end
-  end
-end
