a1c47c7431de7eb7f6b2bc6bb53cd19fc530c32a
Mark Lu <marklu@marklu.net>
2010-11-01 18:50:41 -0700

    Views for trip_participant_can_view_ride_arrangement + refactoring to make them work

--- features/step_definitions/trip_participant_can_view_ride_arrangement.rb
+++ features/step_definitions/trip_participant_can_view_ride_arrangement.rb
@@ -1,5 +1,7 @@
 Given /^there is a trip named "([^"]*)"$/ do |name|
-  Trip.create!(:name => name)
+  Given %{there is a person named "Test Organizer" with email "testorganizer@example.com" and password "Test123!"}
+  organizer = Person.where(:email => "testorganizer@example.com").first
+  Trip.create!(:name => name, :organizer => organizer, :time => Time.now() + 86400, :address => "123 Main St", :city => "Testville", :state => "CA")
 end
 
 Given /^the following people are on the same ride arrangement for "([^"]*)":$/ do |trip_name, participants|
@@ -15,7 +17,7 @@ end
 Given /^the following people do not have a ride arrangement for "([^"]*)":$/ do |trip_name, participants|
   trip = Trip.where(:name => trip_name).first
   participants.hashes.each do |participant|
-    person = Person.where(:email => participant['email'])
+    person = Person.where(:email => participant['email']).first
     if !person.nil?
       trip.arrangements.each do |arrangement|
         arrangement.participants.delete person
