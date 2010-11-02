d129f8a6d753b50250bfb7b660f1d97c6a5d1e9e
jasonku <jasontku@gmail.com>
2010-11-01 17:02:56 -0700

    starting person specs

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe Person do
-  
+
+  before(:each) do
+#    @current_user = mock_model(Person, {:id => 1})
+  end
+
+  describe "when validating a person" do
+    it "should not allow a person with no name" do
+      @no_name_attributes = {
+
+      }
+      @person = Person.new(@no_name_attributes)
+      @person.should_not be_valid
+    end
+
+
+
+  end
+
+
 end
