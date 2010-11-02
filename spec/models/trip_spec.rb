dc37e59615204e0ffea1915327bae8554965850c
Mark Lu <marklu@marklu.net>
2010-11-02 00:34:51 -0700

    Updated trip model spec.

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -3,6 +3,31 @@ require 'spec_helper'
 describe Trip do
   before(:each) do
     @current_user = mock_model(Person, {:id => 1})
+    @past_trip_attributes = {
+      :organizer => @trip_organizer,
+      :name => "Some Trip",
+      :time => Time.now() - 1000,
+      :address => "1234 Main St",
+      :city => "Berkeley",
+      :state => "CA"
+    }
+    @future_trip_attributes = {
+      :organizer => @trip_organizer,
+      :name => "Some Trip",
+      :time => Time.now() + 1000,
+      :address => "1234 Main St",
+      :city => "Berkeley",
+      :state => "CA"
+    }
+    @person_attributes = {
+      :name => "Test User",
+      :address => "1234 Main St",
+      :city => "Testville",
+      :state => "CA",
+      :phone => "1234567890",
+      :email => "test@example.com",
+      :password => "Test123!"
+    }
   end
   
   describe "when validating a trip" do
@@ -139,5 +164,54 @@ describe Trip do
     }
     
     Trip.create(@valid_attributes).should be_true
+  end
+
+  describe "when checking upcoming trips for a person" do
+    it "should return all upcoming trips that the person is a participant in" do
+      @trip_organizer = Person.create!(@person_attributes.update({:name => "Trip Organizer", :email => "organizer@example.com"}))
+      @participant = Person.create!(@person_attributes.update({:name => "Trip Participant", :email => "participant@example.com"}))
+      
+      @first_trip = Trip.create!(@future_trip_attributes.update({:organizer => @trip_organizer}))
+      @second_trip = Trip.create!(@future_trip_attributes.update({:organizer => @trip_organizer}))
+
+      @first_trip.participants.push @participant
+      @second_trip.participants.push @participant
+
+      @first_trip.stub!(:upcoming?).and_return true
+      @second_trip.stub!(:upcoming?).and_return true
+
+      @upcoming_trips = Trip.upcoming_for(@participant)
+      @upcoming_trips.should include @first_trip
+      @upcoming_trips.should include @second_trip
+    end
+    
+    it "should not return trips that are not upcoming" do
+      @participant = Person.create!(@person_attributes.update({:name => "Trip Participant", :email => "participant@example.com"}))
+      @past_trip = Trip.create!(@past_trip_attributes)
+      @past_trip.participants.push @participant
+      
+      @past_trip.stub!(:upcoming?).and_return false
+
+      Trip.upcoming_for(@participant).should_not include @past_trip
+    end
+
+    it "should not return trips that the person is not participating in" do
+      @participant = Person.create!(@person_attributes.update({:name => "Trip Participant", :email => "participant@example.com"}))
+      @trip = Trip.create!(@future_trip_attributes)
+      @trip.participants.stub!(:include?).with(@participant).and_return false
+      
+      Trip.upcoming_for(@participant).should_not include @trip
+    end
+  end
+  
+  describe "when determining if a trip is upcoming" do
+    it "should be upcoming when the trip is in the future" do
+      @trip = Trip.new(@future_trip_attributes)
+      @trip.upcoming?.should be_true
+    end
+    it "should not be upcoming when the trip is in the past" do
+      @trip = Trip.new(@past_trip_attributes)
+      @trip.upcoming?.should be_false
+    end
   end
 end
