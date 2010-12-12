0628d7a828ecf8bf7f5814b54a32f01e2a83035f
jasonku <jasontku@gmail.com>
2010-12-12 14:12:30 -0800

    change base url in invitation email for deployment

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-ActionController::AbstractRequest.relative_url_root = "rideshare.heroku.com/"
-
 # Initialize the rails application
 Rides::Application.initialize!
