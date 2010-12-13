206331e1a6e708d58aca95d21bdd45be63c8c3dd
jasonku <jasontku@gmail.com>
2010-12-12 23:48:23 -0800

    remove overriding smtp_settings so we can use SendGrid addon

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,11 +1,11 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => "cs169rides.com",
-  :user_name            => "cs169rides",
-  :password             => "selenium",
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-}
+#ActionMailer::Base.smtp_settings = {
+#  :address              => "smtp.gmail.com",
+#  :port                 => 587,
+#  :domain               => "cs169rides.com",
+#  :user_name            => "cs169rides",
+#  :password             => "selenium",
+#  :authentication       => "plain",
+#  :enable_starttls_auto => true
+#}
 
 ActionMailer::Base.default_url_options[:host] = "rideshare.heroku.com"
